9a4f614378faf9fc58bce2e1ed84f248280c6a84
Yingqi Chen <chanwk18@hotmail.com>
2019-10-25 05:10:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+fuction writeCards(array,specialDay){
+  newArray = []
+  for (let i = 0, i < array.length, i++){
+    newArray = `Thank you, ${array[i]}, for the wonderful ${specialDay} gift!`
+  }
+}
